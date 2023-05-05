b19b4382ffacd6fd4db1db1d41b798c1d1cd3999
PhilippSmolka <rogerdrager@gmail.com>
2023-05-05 13:49:45 +0200

    Extra command, first 40 chars of line as filename

--- main.ts
+++ main.ts
@@ -1,137 +1,95 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, MarkdownView} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class ExtractNotePlugin extends Plugin {
+  async onload() {
+    console.log('Extract Note plugin loaded.');
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+    this.addCommand({
+      id: 'extract-note',
+      name: 'Extract Note',
+      callback: () => this.extractNote(),
+      hotkeys: [],
+    });
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+    this.addCommand({
+      id: 'extract-note-2',
+      name: 'Extract Note with first 40 characters as filename',
+      callback: () => this.extractNote2(),
+      hotkeys: [],
+    });
+  }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  async extractNote() {
+    const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    if (!activeView) {
+      return;
+    }
 
-	async onload() {
-		await this.loadSettings();
+    let editor = app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+    if (editor == null) {
+      return;
+    }
+    const cursor = editor.getCursor();
+    const line = cursor.line;
+    const lastline = editor.lastLine();
+    const ch = 0;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    const afterCursor = editor.getRange({line, ch: 0}, {line: lastline, ch: Infinity});
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    const filename = await this.generateUniqueFilename(activeView.file.path, 'unknown');
+    const fileContents = afterCursor;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    await this.app.vault.create(filename, fileContents);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    const start = { line, ch: 0 };
+    const end = { line: lastline, ch: Infinity };
+    editor.replaceRange("", start, end);
+  }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  async extractNote2() {
+    const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    if (!activeView) {
+      return;
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    let editor = app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+    if (editor == null) {
+      return;
+    }
+    const cursor = editor.getCursor();
+    const line = cursor.line;
+    const lastline = editor.lastLine();
+    const ch = 0;
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    const lineText = editor.getLine(line).substring(0, 40);
+    const sanitizedFilename = lineText.replace(/[^a-zA-Z0-9-_]+/g, '_').replace(/_{2,}/g, '_').replace(/(^_|_$)/g, '');
 
-	onunload() {
+    const afterCursor = editor.getRange({line, ch: 0}, {line: lastline, ch: Infinity});
 
-	}
+    const filename = await this.generateUniqueFilename(activeView.file.path, sanitizedFilename);
+    const fileContents = afterCursor;
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    await this.app.vault.create(filename, fileContents);
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+    const start = { line, ch: 0 };
+    const end = { line: lastline, ch: Infinity };
+    editor.replaceRange("", start, end);
+  }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+  async generateUniqueFilename(path: string, basename: string): Promise<string> {
+    const activeFile = this.app.workspace.getActiveFile();
+    const parentFolderPath = this.app.vault.getAbstractFileByPath(activeFile?.parent?.path!)?.path;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    const sanitizedBasename = basename.replace(/[^a-zA-Z0-9-_]+/g, '_').replace(/_{2,}/g, '_').replace(/(^_|_$)/g, '');
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    let index = 1;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    while (true) {
+      const filename = `${sanitizedBasename}_${index}.md`;
+      if (!(await this.app.vault.adapter.exists(parentFolderPath + '/' + filename))) {
+        return parentFolderPath + '/' + filename;
+      }
+      index++;
+    }
+  }
 }
